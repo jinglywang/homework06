ba1d68208c1db262ad5e6b6eaf50b53063ef0d95
Jing Wang <jinglywang@gmail.com>
2019-06-13 23:05:35 -0700

    check in homework 06

---
+++ BirthdayCard/BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+			return View();
+        }
+
+		[HttpGet]
+		public ActionResult BirthdayCardForm()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult BirthdayCardForm(Models.BirthCardInfo birthdayCardInfo)
+		{
+			if (ModelState.IsValid)
+			{
+				return View("CardSent", birthdayCardInfo);
+			}
+			else
+			{
+				return View();
+			}
+		}
+	}
+}
